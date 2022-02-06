39b781717d172675f3d84b334c463227c62c250a
Neyeus <81183043+Neyeus@users.noreply.github.com>
2022-02-06 10:45:47 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function ChangeSrc(source){
+    document.getElementById("video").src = source;
+    document.getElementById("lecture").load();
+    window.alert("Veuillez désactiver votre bloqueur de publicité ")
+
+}
